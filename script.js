8721ba42f2e11668ac71703c3c293303ffe1c8fe
OilyPotato819 <90284101+OilyPotato819@users.noreply.github.com>
2021-10-27 15:41:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ let numCodEl = document.getElementById("num-cod");
 let numSalmonEl = document.getElementById("num-salmon");
 let numTropicalEl = document.getElementById("num-tropical");
 let numPufferEl = document.getElementById("num-puffer");
+let plus5Btn = document.getElementById("plus5");
+let until200Btn = document.getElementById("until200");
 
 // Global Variables
 let character = "Steve";
@@ -23,102 +25,114 @@ steveImgEl.addEventListener("click", selectSteve);
 alexImgEl.addEventListener("click", selectAlex);
 villagerImgEl.addEventListener("click", selectVillager);
 fishBtnEl.addEventListener("click", fishOnce);
+plus5Btn.addEventListener("click", plus5);
+until200Btn.addEventListener("click", until200)
 
 // Event Functions
 function selectSteve() {
-    // Update Active Border
-    steveImgEl.classList.add("active");
-    alexImgEl.classList.remove("active");
-    villagerImgEl.classList.remove("active");
+  // Update Active Border
+  steveImgEl.classList.add("active");
+  alexImgEl.classList.remove("active");
+  villagerImgEl.classList.remove("active");
 
-    // Update Character Selection Variable
-    character = "Steve";
+  // Update Character Selection Variable
+  character = "Steve";
 }
 
 function selectAlex() {
-    // Update Active Border
-    alexImgEl.classList.add("active");
-    steveImgEl.classList.remove("active");
-    villagerImgEl.classList.remove("active");
+  // Update Active Border
+  alexImgEl.classList.add("active");
+  steveImgEl.classList.remove("active");
+  villagerImgEl.classList.remove("active");
 
-    // Update Character Selection Variable
-    character = "Alex";
+  // Update Character Selection Variable
+  character = "Alex";
 }
 
 function selectVillager() {
-    // Update Active Border
-    villagerImgEl.classList.add("active");
-    alexImgEl.classList.remove("active");
-    steveImgEl.classList.remove("active");
+  // Update Active Border
+  villagerImgEl.classList.add("active");
+  alexImgEl.classList.remove("active");
+  steveImgEl.classList.remove("active");
 
-    // Update Character Selection Variable
-    character = "Villager";
+  // Update Character Selection Variable
+  character = "Villager";
 }
 
 function fishOnce() {
-    // Test Current Character
-    if (character === "Steve") {
-        // Use Steve Probability Distribution
-        let randNum = Math.random();
-        console.log(randNum)
-        if (randNum < 0.7) {
-            numCod++;
-            numCodEl.innerHTML = numCod;
-            imgResultEl.src = "img/Raw-Cod.png";
-        } else if (randNum < 0.9) {
-            numSalmon++;
-            numSalmonEl.innerHTML = numSalmon;
-            imgResultEl.src = "img/Raw-Salmon.png";
-        } else if (randNum < 0.95) {
-            numTropical++;
-            numTropicalEl.innerHTML = numTropical;
-            imgResultEl.src = "img/Tropical-Fish.png";
-        } else {
-            numPuffer++;
-            numPufferEl.innerHTML = numPuffer;
-            imgResultEl.src = "img/Pufferfish.png";
-        }
-    } else if (character === "Alex") {
-        // Use Alex Probability Distribution
-        let randNum = Math.random();
-        console.log(randNum)
-        if (randNum < 0.1) {
-            numCod++;
-            numCodEl.innerHTML = numCod;
-            imgResultEl.src = "img/Raw-Cod.png";
-        } else if (randNum < 0.2) {
-            numSalmon++;
-            numSalmonEl.innerHTML = numSalmon;
-            imgResultEl.src = "img/Raw-Salmon.png";
-        } else if (randNum < 0.5) {
-            numTropical++;
-            numTropicalEl.innerHTML = numTropical;
-            imgResultEl.src = "img/Tropical-Fish.png";
-        } else {
-            numPuffer++;
-            numPufferEl.innerHTML = numPuffer;
-            imgResultEl.src = "img/Pufferfish.png";
-        }
+  // Test Current Character
+  if (character === "Steve") {
+    // Use Steve Probability Distribution
+    let randNum = Math.random();
+    if (randNum < 0.7) {
+      numCod++;
+      numCodEl.innerHTML = numCod;
+      imgResultEl.src = "img/Raw-Cod.png";
+    } else if (randNum < 0.9) {
+      numSalmon++;
+      numSalmonEl.innerHTML = numSalmon;
+      imgResultEl.src = "img/Raw-Salmon.png";
+    } else if (randNum < 0.95) {
+      numTropical++;
+      numTropicalEl.innerHTML = numTropical;
+      imgResultEl.src = "img/Tropical-Fish.png";
     } else {
-        // Use Villager Probability Distribution
-        let randNum = Math.random();
-        console.log(randNum)
-        if (randNum < 0.05) {
-            numCod++;
-            numCodEl.innerHTML = numCod;
-            imgResultEl.src = "img/Raw-Cod.png";
-        } else if (randNum < 0.1) {
-            numSalmon++;
-            numSalmonEl.innerHTML = numSalmon;
-            imgResultEl.src = "img/Raw-Salmon.png";
-        } else if (randNum < 0.3) {
-            numTropical++;
-            numTropicalEl.innerHTML = numTropical;
-            imgResultEl.src = "img/Tropical-Fish.png";
-        } else {
-            numPuffer++;
-            numPufferEl.innerHTML = numPuffer;
-            imgResultEl.src = "img/Pufferfish.png";
-        }
+      numPuffer++;
+      numPufferEl.innerHTML = numPuffer;
+      imgResultEl.src = "img/Pufferfish.png";
+    }
+  } else if (character === "Alex") {
+    // Use Alex Probability Distribution
+    let randNum = Math.random();
+    if (randNum < 0.1) {
+      numCod++;
+      numCodEl.innerHTML = numCod;
+      imgResultEl.src = "img/Raw-Cod.png";
+    } else if (randNum < 0.2) {
+      numSalmon++;
+      numSalmonEl.innerHTML = numSalmon;
+      imgResultEl.src = "img/Raw-Salmon.png";
+    } else if (randNum < 0.5) {
+      numTropical++;
+      numTropicalEl.innerHTML = numTropical;
+      imgResultEl.src = "img/Tropical-Fish.png";
+    } else {
+      numPuffer++;
+      numPufferEl.innerHTML = numPuffer;
+      imgResultEl.src = "img/Pufferfish.png";
+    }
+  } else {
+    // Use Villager Probability Distribution
+    let randNum = Math.random();
+    if (randNum < 0.05) {
+      numCod++;
+      numCodEl.innerHTML = numCod;
+      imgResultEl.src = "img/Raw-Cod.png";
+    } else if (randNum < 0.1) {
+      numSalmon++;
+      numSalmonEl.innerHTML = numSalmon;
+      imgResultEl.src = "img/Raw-Salmon.png";
+    } else if (randNum < 0.3) {
+      numTropical++;
+      numTropicalEl.innerHTML = numTropical;
+      imgResultEl.src = "img/Tropical-Fish.png";
+    } else {
+      numPuffer++;
+      numPufferEl.innerHTML = numPuffer;
+      imgResultEl.src = "img/Pufferfish.png";
+    }
+  }
+}
+
+function plus5() {
+  for (let n = 1; n <= 5; n++) {
+    fishOnce();
+  }
+}
+
+function until200() {
+    let codTarget = numCod + 200;
+    while (numCod < codTarget ) {
+        fishOnce();
     }
 }
